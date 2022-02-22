f3ee30ebdfa26a18619c69ae3d12d62f60b3ef09
f-audrey <f-audrey@live.fr>
2022-02-23 00:40:30 +0100

    architecture test with header

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,7 +3,21 @@ import './style.scss';
 
 // == Composant
 const App = () => (
-  <div className="app" />
+  <div className="app">
+    <header>
+      <div className="test">
+        <div>logo du site</div>
+        <h1>A Pet 4 Life</h1>
+        <div>
+          <button>SIGN IN</button>
+          <button>SIGN UP</button>
+        </div>
+      </div>
+      <nav />
+    </header>
+    <div className="main" />
+    <footer />
+  </div>
 );
 
 // == Export
