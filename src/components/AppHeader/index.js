49356f49614535417e8ac14926fd1b05dd3ffd94
huiitre <yanis.deluca@laposte.net>
2022-02-23 23:05:15 +0100

    new burger vanilla is ok, need a little modification for open animation

--- src/components/AppHeader/index.js
+++ src/components/AppHeader/index.js
@@ -1,42 +1,36 @@
 import './style.scss';
-// import { useEffect } from 'react';
-import Nav from 'src/components/AppHeader/Nav';
 import Header from 'src/components/AppHeader/Header';
-import Burger from 'src/components/AppHeader/Burger';
+import Nav from 'src/components/AppHeader/Nav';
 
-const AppHeader = () =>
-// const state = {
-//   width: 0,
-// };
-// function handleResize() {
-//   state.width = window.innerWidth;
-// }
-// useEffect(
-//   () => {
-//     window.addEventListener('resize', handleResize);
-//   },
-// );
+// import { useEffect } from 'react';
 
-// console.log(state.width);
-
-//* je stocke le résultat de la var "test" dans le state
-//* ensuite, je récupère ce state et j'affiche le composant voulu selon certaines conditions
-
-  (
-    <header className="app-header">
-      {/* {state.width > 600 && (
-        <>
-          <Header />
-          <Nav />
-        </>
-      )}
-      {state.width <= 600 && (
-        <Burger />
-      )} */}
-      <Header />
-      <Nav />
-      <Burger />
-
-    </header>
-  );
+const AppHeader = () => (
+  <header className="app-header">
+    <Header />
+    <Nav />
+  </header>
+  // <nav className="navbar">
+  //   <div className="navbar__logo">Logo</div>
+  //   <ul className="navbar__links">
+  //     <li className="navbar__item">
+  //       <a href="/" className="navbar__link">Accueil</a>
+  //     </li>
+  //     <li className="navbar__item">
+  //       <a href="/" className="navbar__link">Accueil</a>
+  //     </li>
+  //     <li className="navbar__item">
+  //       <a href="/" className="navbar__link">Accueil</a>
+  //     </li>
+  //     <li className="navbar__item">
+  //       <a href="/" className="navbar__link">Connexion</a>
+  //     </li>
+  //     <li className="navbar__item">
+  //       <a href="/" className="navbar__link">Inscription</a>
+  //     </li>
+  //   </ul>
+  //   <button type="button" className="navbar__burger">
+  //     <span className="burger-bar" />
+  //   </button>
+  // </nav>
+);
 export default AppHeader;
