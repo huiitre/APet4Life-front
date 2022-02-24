294c824071835e75aa3050a85bd40c47fb5050f6
huiitre <yanis.deluca@laposte.net>
2022-02-24 15:27:46 +0100

    add button component. Need some style

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,7 +1,9 @@
 import './style.scss';
+import { Button as ButtonSem } from 'semantic-ui-react';
 
 const Button = ({ name, className }) => (
-  <button type="button" className={className}>{name}</button>
+  // <button type="button" className={className}><span>{name}</span></button>
+  <ButtonSem className={className} color="">{name}</ButtonSem>
 );
 
 export default Button;
