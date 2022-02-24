9ba75ee4cfc1410354ad131cdeb4adee7a66db22
huiitre <yanis.deluca@laposte.net>
2022-02-24 16:26:22 +0100

    added classes for connexion, subscribe and disconnect button style

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,9 +1,15 @@
 import './style.scss';
+import PropTypes from 'prop-types'
 import { Button as ButtonSem } from 'semantic-ui-react';
 
 const Button = ({ name, className }) => (
-  // <button type="button" className={className}><span>{name}</span></button>
-  <ButtonSem className={className} color="">{name}</ButtonSem>
+  <button type="button" className={className}><span>{name}</span></button>
+  // <ButtonSem className={className} color="">{name}</ButtonSem>
 );
+
+Button.propTypes = {
+  name: PropTypes.string.isRequired,
+  className: PropTypes.string.isRequired,
+};
 
 export default Button;
