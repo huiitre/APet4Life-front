2245e1047d7987daa0be8cbb4815e3daada9d743
huiitre <yanis.deluca@laposte.net>
2022-02-25 14:42:19 +0100

    carousel on home is done, need maybe a little style ajust

--- src/components/MainCarousel/index.js
+++ src/components/MainCarousel/index.js
@@ -2,32 +2,46 @@ import './style.scss';
 
 //* import data temporaire pour les images
 import pictures from 'src/data/pictures';
+import carousel1 from 'src/assets/img/carousel1.jpg';
+import carousel2 from 'src/assets/img/carousel2.jpg';
+import carousel3 from 'src/assets/img/carousel3.jpg';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/bundle';
 import 'swiper/css/pagination';
-import { Pagination } from 'swiper';
+import 'swiper/css/navigation';
+import { Pagination, Navigation, EffectFade } from 'swiper';
 
 const MainCarousel = () => (
-  <div className="swiper-div">
+  <div className="swiper">
     <Swiper
-      className="mySwipper2 swiper-v"
-      spaceBetween={50}
+      className="mySwipper"
       slidesPerView={1}
+      spaceBetween={30}
+      loop
+      navigation
+      effect="fade"
       // onSlideChange={() => console.log('slide change')}
       // onSwiper={(swiper) => console.log(swiper)}
       pagination={{
         clickable: true,
       }}
-      modules={[Pagination]}
+      modules={[EffectFade, Pagination, Navigation]}
     >
       {/* <SwiperSlide><img src="" alt="random" /></SwiperSlide> */}
-      {pictures.map((item) => (
-        <SwiperSlide>
-          <img src={item} alt="random" />
-        </SwiperSlide>
-      ))}
+      <SwiperSlide>
+        <img className="swiper__img" src={carousel1} alt="carousel1" />
+        <p className="swiper__content">Le site qui références les associations animales dans ta région</p>
+      </SwiperSlide>
+      <SwiperSlide>
+        <img className="swiper__img" src={carousel2} alt="carousel2" />
+        <p className="swiper__content">Tu cherches à adopter un animal en particulier ?</p>
+      </SwiperSlide>
+      <SwiperSlide>
+        <img className="swiper__img" src={carousel3} alt="carousel3" />
+        <p className="swiper__content">Le formulaire de recherche plus bas t'aidera à trouver l'association la plus proche de chez toi</p>
+      </SwiperSlide>
     </Swiper>
   </div>
 );
--- src/data/pictures.js
+++ src/data/pictures.js
@@ -1,14 +1,19 @@
 const pictures = [
-  'https://picsum.photos/200',
-  'https://picsum.photos/200',
-  'https://picsum.photos/200',
-  'https://picsum.photos/200',
-  'https://picsum.photos/200',
-  'https://picsum.photos/200',
-  'https://picsum.photos/200',
-  'https://picsum.photos/200',
-  'https://picsum.photos/200',
-  'https://picsum.photos/200',
+  {
+    content: 'Titre 1',
+    src: './src/assets/img/carousel1.jpg',
+    alt: 'carousel1',
+  },
+  {
+    content: 'Titre 2',
+    src: 'src/assets/img/carousel2.jpg',
+    alt: 'carousel2',
+  },
+  {
+    content: 'Titre 3',
+    src: 'src/assets/img/carousel3.jpg',
+    alt: 'carousel3',
+  },
 ];
 
 export default pictures;
