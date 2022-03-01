1e6f176d22e593655d316bf5fde65ddbfdd815f1
f-audrey <f-audrey@live.fr>
2022-03-01 14:37:08 +0100

    position of login form ok

--- src/components/AppHeader/Header/index.js
+++ src/components/AppHeader/Header/index.js
@@ -1,5 +1,6 @@
 import './style.scss';
 // import logo from 'src/assets/img/logo-pet.jpg';
+import FormLogin from 'src/components/Forms/FormLogin';
 import Button from 'src/components/Button';
 import title from 'src/assets/img/title.png';
 
@@ -8,6 +9,7 @@ const Header = () => (
     {/* <img className="header__logo" src={logo} alt="logo" /> */}
     {/* <h1 className="header__title">A Pet 4 Life</h1> */}
     <img className="header__title" src={title} alt="title" />
+    <FormLogin />
     <div className="header__user">
       <Button type="" name="Connexion" className="btn--signin" />
       <Button type="" name="Inscription" className="btn--signup" />
--- src/components/Forms/FormLogin/index.js
+++ src/components/Forms/FormLogin/index.js
@@ -1,7 +1,36 @@
 import './style.scss';
+import Field from 'src/components/Forms/Field';
+import Button from 'src/components/Button';
 
-const FormLogin = () => (
-  <div className="" />
-);
+const FormLogin = () => {
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log('submit');
+  };
+
+  return (
+    <div className="form__login">
+      <form onSubmit={onSubmit} className="form__login-container">
+        <Field
+          type="text"
+          placeholder="Email"
+          name="mail"
+          className="form__login-field"
+        />
+        <Field
+          type="password"
+          placeholder="Mot de passe"
+          name="password"
+          className="form__login-field"
+        />
+        <Button
+          type="submit"
+          name="Envoyer"
+          className="form__login-button"
+        />
+      </form>
+    </div>
+  );
+};
 
 export default FormLogin;
