95368c8f1d3b186084a8a528289ed807612bcf66
huiitre <yanis.deluca@laposte.net>
2022-03-01 19:18:01 +0100

    add axios requests for search assoc by region, department or zipcode, insert result on state.

--- src/store/middlewares/association.js
+++ src/store/middlewares/association.js
@@ -1,63 +1,90 @@
+/* eslint-disable spaced-comment */
 import axios from 'axios';
 import {
   SEND_SEARCH_QUERY_BY_ZIPCODE,
   SEND_SEARCH_QUERY_BY_DEPARTMENT,
   SEND_SEARCH_QUERY_BY_REGION,
+  insertSearchResultToState,
 } from '../actions/associations';
 
 const associationMiddleware = (store) => (next) => (action) => {
   switch (action.type) {
+    //todo recherche assoc par code postal
     case SEND_SEARCH_QUERY_BY_ZIPCODE:
-      // const state = store.getState();
-      // const { zipcode } = state.associations;
-      // console.log(zipcode);
-      axios.get('http://localhost:3000/api/user/search', {
-        zipcode,
-      })
-        .then((response) => {
-          console.log('success', response);
-        })
-        .catch((error) => {
-          console.log('error', error);
-        });
+      {
+        const {
+          associations: {
+            formAssoc: { zipcode },
+          },
+        } = store.getState();
+
+        const config = {
+          geolocation: 'zipcode',
+          responseLocation: zipcode,
+        };
+
+        axios
+          .post('http://localhost:3000/api/user/search', config)
+          .then((response) => {
+            store.dispatch(insertSearchResultToState(response.data));
+          })
+          .catch((error) => {
+            console.log('error', error);
+          });
+      }
 
       next(action);
       break;
 
+    //todo recherche assoc par département
     case SEND_SEARCH_QUERY_BY_DEPARTMENT:
+      {
+        const {
+          associations: {
+            formAssoc: { department },
+          },
+        } = store.getState();
 
-      { const state = store.getState();
-        const { associations: { formAssoc: { department } } } = state;
-        console.log(`on est dans le middleware region: ${region}`);
+        const config = {
+          geolocation: 'department',
+          responseLocation: department,
+        };
 
-        axios.get('http://localhost:3000/api/user/search', {
-          department,
-        })
+        axios
+          .post('http://localhost:3000/api/user/search', config)
           .then((response) => {
-            console.log('success', response);
+            store.dispatch(insertSearchResultToState(response.data));
           })
           .catch((error) => {
             console.log('error', error);
-          }); }
-
+          });
+      }
       next(action);
       break;
 
+    //todo recherche assoc par région
     case SEND_SEARCH_QUERY_BY_REGION:
-      { const state = store.getState();
-        const { associations: { formAssoc: { region } } } = state;
-        console.log(`on est dans le middleware region: ${region}`);
+      {
+        const {
+          associations: {
+            formAssoc: { region },
+          },
+        } = store.getState();
 
-        axios.get('http://localhost:3000/api/user/search', {
-          region,
-        })
+        const config = {
+          geolocation: 'region',
+          responseLocation: region,
+        };
+
+        axios
+          .post('http://localhost:3000/api/user/search', config)
           .then((response) => {
-            console.log('success', response);
+            store.dispatch(insertSearchResultToState(response.data));
           })
           .catch((error) => {
             console.log('error', error);
-          }); }
-
+          });
+      }
       next(action);
       break;
 
