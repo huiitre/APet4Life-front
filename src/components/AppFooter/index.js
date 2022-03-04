7f0edac098f42450a5eb64841c31b922ce30a095
huiitre <yanis.deluca@laposte.net>
2022-03-04 22:27:40 +0100

    fix footer fixed and appHeader fixed (just on search and home page, need fix for association/slug page)

--- src/components/AppFooter/index.js
+++ src/components/AppFooter/index.js
@@ -1,45 +1,41 @@
 //* on importe la feuille de style, comme dans tous les composants
-import './style.scss';
+import "./style.scss";
 //* on importe les logo des réseaux sociaux
-import logoFb from 'src/assets/img/facebook.png';
-import logoInsta from 'src/assets/img/insta.png';
-import logoTwitter from 'src/assets/img/twitter.png';
+import logoFb from "src/assets/img/facebook.png";
+import logoInsta from "src/assets/img/insta.png";
+import logoTwitter from "src/assets/img/twitter.png";
 
 const AppFooter = () => (
-
-  <footer className="app-footer">
-    <div className="footer">
-      <div className="footer__links">
-        <p>A Pet 4 Life 2022</p>
-        <a href="">Mentions légales et CGU</a>
-        <a href="">Nous contacter</a>
-      </div>
-      <div className="footer__socials">
-        <a href="">
-          <img
-            className="footer__socials-item"
-            src={logoFb}
-            alt="logo facebook"
-          />
-        </a>
-        <a href="">
-          <img
-            className="footer__socials-item"
-            src={logoInsta}
-            alt="logo instagram"
-          />
-        </a>
-        <a href="">
-          <img
-            className="footer__socials-item"
-            src={logoTwitter}
-            alt="logo twitter"
-          />
-        </a>
-      </div>
+  <footer className="footer">
+    <div className="footer__links">
+      <p>A Pet 4 Life 2022</p>
+      <a href="">Mentions légales et CGU</a>
+      <a href="">Nous contacter</a>
+    </div>
+    <div className="footer__socials">
+      <a href="">
+        <img
+          className="footer__socials-item"
+          src={logoFb}
+          alt="logo facebook"
+        />
+      </a>
+      <a href="">
+        <img
+          className="footer__socials-item"
+          src={logoInsta}
+          alt="logo instagram"
+        />
+      </a>
+      <a href="">
+        <img
+          className="footer__socials-item"
+          src={logoTwitter}
+          alt="logo twitter"
+        />
+      </a>
     </div>
   </footer>
-
 );
 
 export default AppFooter;
