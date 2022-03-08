d6579614ce421f3ad3a34e4e6e34fd779e757313
huiitre <yanis.deluca@laposte.net>
2022-03-08 12:58:10 +0100

    create associations component (page)

---
+++ src/components/associations/index.js
@@ -0,0 +1,13 @@
+import './style.scss';
+import Page from 'src/components/Page';
+import AssocList from 'src/components/AssocList';
+
+const Associations = () => (
+  <Page>
+    <section className="associations">
+      <AssocList />
+    </section>
+  </Page>
+);
+
+export default Associations;
