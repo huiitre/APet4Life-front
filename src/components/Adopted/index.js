ec17c76e209e478d560ff8be2bac2a04b0ef18e0
huiitre <yanis.deluca@laposte.net>
2022-03-11 14:20:00 +0100

    add new vanilla component, and dynamisation with data, responsive is done

--- src/components/Adopted/index.js
+++ src/components/Adopted/index.js
@@ -1,13 +1,54 @@
-import './style.scss';
-import Page from 'src/components/Page';
-import Card from './card';
+import "./style.scss";
+import Page from "src/components/Page";
+import { Card, Icon, Image } from "semantic-ui-react";
+import adopted from "../../data/adopted";
+import Separator from "src/components/Separator";
 
-const Adopted = () => (
-  <Page>
-    <section className="adopted">
-      <Card />
-    </section>
-  </Page>
-);
+const Adopted = () => {
+  return (
+    <Page className="adopted-page">
+      <section className="adopted">
+        {/* <Card.Group>
+          {adopted.map((item) => (
+            <Card>
+              <Image
+                src={require(`../../assets/img/adopted/${item.picture}`)}
+                wrapped
+                ui={false}
+              />
+              <Card.Content>
+                <Card.Header>{item.name}</Card.Header>
+                <Card.Meta>
+                  <span className="date">Adopté par</span>
+                </Card.Meta>
+                <Card.Description>
+                  {item.owner}
+                </Card.Description>
+              </Card.Content>
+            </Card>
+          ))}
+        </Card.Group> */}
+        {adopted.map((item) => (
+          <>
+            <div key={item.id} className="element">
+              <div className="element__picture">
+                <img
+                  src={require(`../../assets/img/adopted/${item.picture}`)}
+                />
+              </div>
+              <div className="element__name">
+                <h3>{item.name}</h3>
+              </div>
+              <div className="element__owner">
+                Adopté par <span className="owner">{item.owner}</span>
+              </div>
+            </div>
+            <Separator />
+          </>
+        ))}
+      </section>
+    </Page>
+  );
+};
 
 export default Adopted;
