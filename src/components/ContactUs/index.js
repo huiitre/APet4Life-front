dfb2f865b08b15c4f8c56a17b7f311b525c40918
f-audrey <f-audrey@live.fr>
2022-03-11 16:12:42 +0100

    team page done - will try to style form with column shape

--- src/components/ContactUs/index.js
+++ src/components/ContactUs/index.js
@@ -1,44 +1,77 @@
 import "./style.scss";
 import Page from "src/components/Page";
+// import FormContact from "../Forms/FormContact";
 import backgroundContact from 'src/assets/img/team-background.jpg';
-import exemple1 from 'src/assets/img/exemple1.jpg';
-import exemple2 from 'src/assets/img/exemple2.jpg';
-import exemple3 from 'src/assets/img/exemple3.jpg';
-import exemple4 from 'src/assets/img/exemple4.jpg';
+import Field from 'src/components/Forms/Field';
+import TextArea from 'src/components/Forms/TextArea';
+import Button from 'src/components/Button';
 
 const ContactUs = () => {
   console.log('here')
+
+  //* fonction onSubmit, pour envoyer un mail à l'assoc via ce formulaire (pas implémenté pour l'instant, juste un event.preventDefault())
+  const handleSubmitContact = (evt) => {
+    evt.preventDefault();
+  };
   
+  const handleClickButton = () => {
+    console.log('handleClickButton')
+  }
+
   return (
     <Page>
       <div className="contact-container">
-        <div className="contact-title">La Dream Team</div>
+        <p className="contact-title">~ OUR DREAM TEAM ~</p>
         <div className="contact">
           <div className="contact__team">
             <div className="contact__team-item">
-              <div className="contact__team-item-name">YANIS</div>
-              <div className="contact__team-item-title">Développeur Frontend</div>
+              <p className="contact__team-item-name">YANIS</p>
+              <p className="contact__team-item-title">Développeur Frontend</p>
               <div className="contact__team-picture picture1"></div>
             </div>
             <div className="contact__team-item">
-              <div className="contact__team-item-name">SAMANTHA</div>
-              <div className="contact__team-item-title">Développeuse Backend</div>
+              <p className="contact__team-item-name">SAMANTHA</p>
+              <p className="contact__team-item-title">Développeuse Backend</p>
               <div className="contact__team-picture picture2"></div>
             </div>
             <div className="contact__team-item">
-              <div className="contact__team-item-name">MORGANE</div>
-              <div className="contact__team-item-title">Développeuse Backend</div>
+              <p className="contact__team-item-name">MORGANE</p>
+              <p className="contact__team-item-title">Développeuse Backend</p>
               <div className="contact__team-picture picture3"></div>
             </div>
             <div className="contact__team-item">
-              <div className="contact__team-item-name">AUDREY</div>
-              <div className="contact__team-item-title">Développeuse Frontend</div>
+              <p className="contact__team-item-name">AUDREY</p>
+              <p className="contact__team-item-title">Développeuse Frontend</p>
               <div className="contact__team-picture picture4"></div>
             </div>
           </div>
 
           <div className="contact__form">
-          Le formulaire de contact
+            <p className="contact__form-title">Ecrivez-nous !</p>
+            <form onSubmit={handleSubmitContact} className={`form-contact-team`}>
+              <Field
+                type="email"
+                placeholder="Email"
+                name="mail"
+                className="contact__form-field"
+              />
+              <Field
+                type="text"
+                placeholder="Objet"
+                name="object"
+                className="contact__form-field"
+              />
+              <TextArea
+                className="contact__form-textarea"
+                placeholder="Votre message ici ..."
+              />
+              <Button
+                type="submit"
+                name="Envoyer"
+                onClick={handleClickButton}
+                className="btn--submit-contact-team"
+              />
+            </form>
           </div>
 
           <img className="contact__background" src={backgroundContact} alt="image fond d'écran" />
