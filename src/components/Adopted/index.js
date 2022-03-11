10c55bd8e1a25f5e6cb84a34b53d2b3d65bd7a23
huiitre <yanis.deluca@laposte.net>
2022-03-11 17:41:17 +0100

    adopted page is done with lightbox

--- src/components/Adopted/index.js
+++ src/components/Adopted/index.js
@@ -1,52 +1,55 @@
 import "./style.scss";
 import Page from "src/components/Page";
-import { Card, Icon, Image } from "semantic-ui-react";
 import adopted from "../../data/adopted";
 import Separator from "src/components/Separator";
+import Lightbox from "react-image-lightbox";
+import "react-image-lightbox/style.css"; // This only needs to be imported once in your app
+import { useState } from "react";
 
 const Adopted = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [currentPicture, setCurrentPicture] = useState();
+
+  const openPicture = (evt) => {
+    console.log("open picture", evt.target.src);
+    setIsOpen(true)
+    setCurrentPicture(evt.target.src);
+    
+  };
+  console.log(currentPicture);
   return (
     <Page className="adopted-page">
-      <section className="adopted">
-        {/* <Card.Group>
-          {adopted.map((item) => (
-            <Card>
-              <Image
-                src={require(`../../assets/img/adopted/${item.picture}`)}
-                wrapped
-                ui={false}
-              />
-              <Card.Content>
-                <Card.Header>{item.name}</Card.Header>
-                <Card.Meta>
-                  <span className="date">Adopté par</span>
-                </Card.Meta>
-                <Card.Description>
-                  {item.owner}
-                </Card.Description>
-              </Card.Content>
-            </Card>
-          ))}
-        </Card.Group> */}
-        {adopted.map((item) => (
-          <>
-            <div key={item.id} className="element">
-              <div className="element__picture">
-                <img
-                  src={require(`../../assets/img/adopted/${item.picture}`)}
-                />
-              </div>
-              <div className="element__name">
-                <h3>{item.name}</h3>
-              </div>
-              <div className="element__owner">
-                Adopté par <span className="owner">{item.owner}</span>
-              </div>
-            </div>
-            <Separator />
-          </>
-        ))}
-      </section>
+      {isOpen && (
+        <Lightbox
+          mainSrc={currentPicture}
+          onCloseRequest={() => setIsOpen(false)}
+        />
+      )}
+      {!isOpen && (
+        <section className="adopted">
+          {adopted.map((item) => {
+            return (
+              <>
+                <div key={item.id} className="element">
+                  <div className="element__picture">
+                    <img
+                      src={require(`../../assets/img/adopted/${item.picture}`)}
+                      onClick={openPicture}
+                    />
+                  </div>
+                  <div className="element__name">
+                    <h3>{item.name}</h3>
+                  </div>
+                  <div className="element__owner">
+                    Adopté par <span className="owner">{item.owner}</span>
+                  </div>
+                </div>
+                <Separator className="" />
+              </>
+            );
+          })}
+        </section>
+      )}
     </Page>
   );
 };
