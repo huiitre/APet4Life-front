2313cae3d34c2fe564140312d8d7941986f59765
f-audrey <f-audrey@live.fr>
2022-03-14 13:26:04 +0100

    scroll to top done

--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -1,12 +1,23 @@
 import './style.scss';
 import PropTypes from 'prop-types';
 
+import { useEffect } from "react";
+import { useLocation } from "react-router";
+
 //* composant Page : container réutilisable avec un props children (style CSS : marge de 2 rem)
-const Page = ({ children, className }) => (
+const Page = ({ children, className }) => {
+
+  const location = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
+  return (
   <main className={`page ${className}`}>
     {children}
   </main>
-);
+  )
+  };
 
 Page.propTypes = {
   children: PropTypes.node.isRequired,
