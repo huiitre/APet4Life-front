62ecc012c558c5a87b2bb9ab0637597807270304
f-audrey <f-audrey@live.fr>
2022-03-14 13:36:45 +0100

    scroll to top done with comment

--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -1,12 +1,14 @@
 import './style.scss';
 import PropTypes from 'prop-types';
 
+//* imports react
 import { useEffect } from "react";
 import { useLocation } from "react-router";
 
 //* composant Page : container réutilisable avec un props children (style CSS : marge de 2 rem)
 const Page = ({ children, className }) => {
 
+  //* à chaque fois que la location (page) change, on scroll en haut de page
   const location = useLocation();
   useEffect(() => {
     window.scrollTo(0, 0);
