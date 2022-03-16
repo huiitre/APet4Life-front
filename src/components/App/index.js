adc04fbf02025b2b1ce3abd6ecc7f686adce211a
huiitre <yanis.deluca@laposte.net>
2022-03-16 16:55:22 +0100

    remove dispatch setallassociationsfromapi FROM app/index and add useeffect for dispatch this on associations/index

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -46,7 +46,9 @@ const App = () => {
       //* on effectue l'action grâce à la méthode associée (load...()) qui est dans le dossier store/actions/...
       dispatch(loadRegionsFromApi());
       dispatch(loadDepartmentsFromApi());
-      dispatch(setAllAssociationsFromApi());
+
+      //! en commentaire car on préfère faire l'appel directement depuis la page /associations
+      // dispatch(setAllAssociationsFromApi());
 
       // const { data: stateData, token: stateToken } = useSelector((state) => state.user.currentUser);
 
