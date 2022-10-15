b105a73af9fced09ebdc847e7ccdda96b02108bf
huiitre <yanis.deluca@laposte.net>
2022-10-16 01:35:48 +0200

    process.env n'est pas mis en place, j'ai modifié l'url de l'api prod depuis les middleware

--- src/store/middlewares/user.js
+++ src/store/middlewares/user.js
@@ -29,7 +29,7 @@ let finalURL = "";
 if (process.env.NODE_ENV === "development") {
   finalURL = "http://localhost:3000";
 } else {
-  finalURL = "http://backoffice.apet4life.huiitre.fr";
+  finalURL = "http://api.apet4life.huiitre.fr";
 }
 
 const axiosInstance = axios.create({
